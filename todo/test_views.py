242cd2e303f8d2e416a3cb9162dac171552f9f54
erincunningham7 <>
2023-09-16 11:03:43 +0000

    update second test and add third test

--- todo/test_views.py
+++ todo/test_views.py
@@ -12,3 +12,12 @@ class TestViews(TestCase):
 
 def test_get_add_item_page(self):
     response = self.client.get('/add')
+    self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'todo/add_item.html')
+
+
+def test_get_edit_item_page(self):
+    item = Item.objects.create(name='Test Todo Item')
+    response = self.client.get(f'/edit/{item.id}')
+    self.assertEqual(response.status_code, 200)
+    self.assertTemplateUsed(response, 'todo/edit_item.html')
