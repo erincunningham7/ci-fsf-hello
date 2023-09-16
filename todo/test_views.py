425574b80a528d489e9d3a22a9207423cb3ebcdc
erincunningham7 <>
2023-09-16 11:02:29 +0000

    Create second test

--- todo/test_views.py
+++ todo/test_views.py
@@ -8,3 +8,7 @@ class TestViews(TestCase):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'todo/todo_list.html')
+
+
+def test_get_add_item_page(self):
+    response = self.client.get('/add')
