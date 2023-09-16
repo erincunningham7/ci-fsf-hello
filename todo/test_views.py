69eae843afa2e101e95fac3f9b6182ae5914b1df
erincunningham7 <>
2023-09-16 11:01:12 +0000

    Add imports

--- todo/test_views.py
+++ todo/test_views.py
@@ -1,12 +1,2 @@
 from django.test import TestCase
-
-# Create your tests here.
-
-
-class TestDjango(TestCase):
-
-    # def test_this_thing_works(self):
-    # self.assertEqual(1, 0)
-
-    def test_this_thing_works(self):
-        self.assertEqual(1, 1)
+from .models import Item
