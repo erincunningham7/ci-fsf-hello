e8975e8ea4e64a18a82add9d0ed4bde8c184f32a
erincunningham7 <>
2023-09-16 11:01:57 +0000

    Create first test

--- todo/test_views.py
+++ todo/test_views.py
@@ -1,2 +1,10 @@
 from django.test import TestCase
 from .models import Item
+
+
+class TestViews(TestCase):
+
+    def test_get_todo_list(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'todo/todo_list.html')
