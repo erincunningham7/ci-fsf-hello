1a055fde0790fcd7450cd3e9cc33b9870eafab7a
erincunningham7 <>
2023-09-16 10:57:13 +0000

    complete first test

--- todo/test_forms.py
+++ todo/test_forms.py
@@ -5,3 +5,7 @@ from .forms import ItemForm
 class TestItemForm(TestCase):
 
     def test_item_name_is_required(self):
+        form = ItemForm({'name': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('name', form.errors.keys())
+        self.assertEqual(form.errors['name'][0], 'This field is required.')
